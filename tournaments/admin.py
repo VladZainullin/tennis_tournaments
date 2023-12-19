699fd6f43a0774b8e69438a247977da31dc9f9fb
Vladislav Zainullin <mittoface02@mail.ru>
2023-12-19 21:11:47 +0300

    Добавляет настройку панели администратора

---
+++ tournaments/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from tournaments.models import *
+
+
+class TournamentAdmin(admin.ModelAdmin):
+    list_filter = [f.name for f in Tournament._meta.get_fields() if f.name != 'id']
+    search_fields = [f.name for f in Tournament._meta.get_fields() if f.name != 'id']
+
+
+class TournamentRefereeAdmin(admin.ModelAdmin):
+    list_filter = [f.name for f in TournamentReferee._meta.get_fields() if f.name != 'id']
+    search_fields = [f.name for f in TournamentReferee._meta.get_fields() if f.name != 'id']
+
+
+class TournamentPlayerAdmin(admin.ModelAdmin):
+    list_filter = [f.name for f in TournamentPlayer._meta.get_fields() if f.name != 'id']
+    search_fields = [f.name for f in TournamentPlayer._meta.get_fields() if f.name != 'id']
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_filter = [f.name for f in Game._meta.get_fields() if f.name != 'id']
+    search_fields = [f.name for f in Game._meta.get_fields() if f.name != 'id']
+
+
+admin.site.register(Tournament)
+admin.site.register(Referee)
+admin.site.register(Player)
+admin.site.register(Organizer)
+admin.site.register(TournamentReferee)
+admin.site.register(TournamentPlayer)
+admin.site.register(Game)
